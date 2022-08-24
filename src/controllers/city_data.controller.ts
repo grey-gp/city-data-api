329a9c053336f2194f30b31190ce1b168e5cbf1b
grey-gp <grey.grahamp@gmail.com>
2022-08-24 14:00:50 -0400

    Created helper function to return a cityData element by name

--- src/controllers/city_data.controller.ts
+++ src/controllers/city_data.controller.ts
@@ -6,13 +6,11 @@ export function getAllCities(req: Request, res: Response) {
 };
 
 export function getCityByName(req: Request, res: Response) {
-    return res.status(200).json(cityData.find((elem) => { 
-	return elem.city.toLowerCase() == req.params.name.toLowerCase() 
-    }));
+    return res.status(200).json(findCityElement(req.params.name));
 };
 
 export function filterByLatitude(req: Request, res: Response) {
-    const referenceCity = cityData.find(elem => elem.city.toLowerCase()== req.params.name.toLowerCase());
+    const referenceCity = findCityElement(req.params.name);
     return res.status(200).json(cityData.filter((elem) => {
 	if (referenceCity) {
 	    if (elem.city.toLowerCase() != referenceCity.city.toLowerCase())
@@ -22,6 +20,10 @@ export function filterByLatitude(req: Request, res: Response) {
 	}
     }));
 };
+
+function findCityElement(cityName: string) {
+    return cityData.find(elem => elem.city.toLowerCase() == cityName.toLowerCase());
+}
 
 function absDifference(x: number, y: number): number {
     return Math.abs(Math.abs(x) - Math.abs(y));
